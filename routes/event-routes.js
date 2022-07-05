f21bd0a15b814e8924ad0f5462393b53ed249446
wasadmin <wasadmin@wasadmin.com>
2022-07-05 10:02:20 -0500

    Reinitialized local repo

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -1,7 +1,22 @@
-  // GET /api/events
+import express from 'express'
+var eventRouter = express.Router()
+import customerController from '../controllers/customer-controller.js'
 
+// GET /api/events
+eventRouter.get('/', function(req, res) {
+  customerController.getAllCustomers(req, res)
+})
   // GET /api/events/:event_id
-
+  eventRouter.get('/:event_id', function(req, res) {
+    customerController.getCustomerByID(req, res)
+  })
   // POST /api/events
-
+  eventRouter.post('/', function(req, res) {
+    customerController.postCustomers(req, res)
+  })
   // DELETE /api/events/:event_id
+  eventRouter.delete('/:event_id', function(req, res) {
+    customerController.deleteCustomerByID(req, res)
+  })
+
+  export default customerRouter
--- routes/registration-routes.js
+++ routes/registration-routes.js
@@ -1,7 +1,22 @@
+import express from 'express'
+var registrationRouter = express.Router()
+import customerController from '../controllers/customer-controller.js'
+
 // GET /api/registrations
-
+registrationRouter.get('/', function(req, res) {
+    customerController.getAllCustomers(req, res)
+  })
 // GET /api/registrations/:registrationID
-
+registrationRouter.get('/:registrationID', function(req, res) {
+    customerController.getCustomerByID(req, res)
+  })
 // POST /api/registration
-
+registrationRouter.post('/', function(req, res) {
+    customerController.postCustomer(req, res)
+  })
 // DELETE /api/registrations/:registrationID
+registrationRouter.delete('/:registrationID', function(req, res) {
+    customerController.deleteCustomerByID(req, res)
+  })
+  
+  export default customerRouter
